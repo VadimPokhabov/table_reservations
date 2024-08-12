f7957856f7ecbbee3fae86e767f5dbf0342f337f
Vadim <VadimPokhabov@mail.ru>
2024-08-12 18:21:50 +0700

    Add view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    """
+    ViewSet для модели User
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
+
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
+
+
