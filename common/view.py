6eadb27b783ada857a3a9156d81a54f7f41adb6c
Vadim <VadimPokhabov@mail.ru>
2024-08-19 13:07:16 +0700

    Add class StyleFormMixin

---
+++ common/view.py
@@ -0,0 +1,25 @@
+from django.forms import BooleanField
+
+
+class StyleFormMixin:
+    """
+    Миксин для стилизации форм.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+# class StyleFormMixin:
+#     """
+#     Миксин для добавления стилей в контекст
+#     """
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         for field_name, field in self.fields.items():
+#             field.widget.attrs['class'] = 'form-control'
