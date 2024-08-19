15ef85245368038cc5a768f2f10ade2e531b58ed
Vadim <VadimPokhabov@mail.ru>
2024-08-19 13:12:40 +0700

    Add users admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
+from django.apps import AppConfig
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+admin.site.register(User)
+
+
+class UserConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
