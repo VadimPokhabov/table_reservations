928b5d981514fbecaf6768b589f7642f3c570112
Vadim <VadimPokhabov@mail.ru>
2024-08-19 13:06:41 +0700

    Add users views.py

--- users/views.py
+++ users/views.py
@@ -1,26 +1,80 @@
-from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet
+import random
+import secrets
+import string
 
+from django.contrib.auth.views import LoginView
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView
+
+from users.forms import UserRegisterForm, ProfileForm, UserLoginForm
 from users.models import User
-from users.serializers import UserSerializer
+from config.settings import EMAIL_HOST_USER
 
 
-class UserViewSet(ModelViewSet):
-    """
-    ViewSet для модели User
-    """
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
 
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def perform_create(self, serializer):
-        user = serializer.save()
-        user.set_password(user.password)
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
         user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email_confirm/{token}/'
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Привет, перейди по ссылке для подтверждения почты {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
 
-    def get_permissions(self):
-        if self.action == "create":
-            self.permission_classes = [AllowAny]
-        return super().get_permissions()
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
 
 
+def reset_password(request):
+    context = {
+        "success_message": "Пароль успешно сброшен. Новый пароль был отправлен на ваш адрес электронной почты.",
+    }
+    if request.method == "POST":
+        email = request.POST.get("email")
+        user = get_object_or_404(User, email=email)
+        characters = string.ascii_letters + string.digits
+        characters_list = list(characters)
+        random.shuffle(characters_list)
+        password = "".join(characters_list[:10])
+        user.set_password(password)
+        user.save()
+        send_mail(
+            subject="Восстановление пароля",
+            message=f"Здравствуйте, вы запрашивали обновление пароля. Ваш новый пароль: {password}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return render(request, "users/reset_password.html", context)
+    else:
+        return render(request, "users/reset_password.html")
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = ProfileForm
+    success_url = reverse_lazy("users:profile")
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
