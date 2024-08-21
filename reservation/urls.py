40bf573893d272b275c9f8eb6fbc63ea5b253076
Vadim <VadimPokhabov@mail.ru>
2024-08-21 16:48:24 +0700

    Add contacts

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from reservation.apps import ReservationConfig
 from reservation.views import TableTemplateView, TableListView, TableDetailView, TableCreateView, TableDeleteView, \
-    TableUpdateView, AboutRestoTemplateView, ReservationCreateView
+    TableUpdateView, AboutRestoTemplateView, ReservationCreateView, ContactsTemplateView
 
 app_name = ReservationConfig.name
-
 
 urlpatterns = [
     path('', TableTemplateView.as_view(), name='base'),
     path('table_list/', TableListView.as_view(), name='table_list'),
     path('reservation/', ReservationCreateView.as_view(), name='reservation'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
     path('about_resto/', AboutRestoTemplateView.as_view(), name='about_resto'),
     path('create/', TableCreateView.as_view(), name='create'),
     path('<int:pk>/', TableDetailView.as_view(), name='view'),
